afe39d514abfe62d5eec0713467ddf90ba4d981a
Eeveeee <fx8300gfe@gmail.com>
2020-11-24 20:50:21 +0300

    adaptive + 2 sliders

---
+++ src/js/sliders.js
@@ -0,0 +1,13 @@
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider'
+tns({
+  container: '.compare-slider_1',
+  items: 1,
+  navPosition: 'bottom',
+  controlsContainer: '.compare-slider_1-controls',
+})
+tns({
+  container: '.compare-slider_2',
+  items: 1,
+  navPosition: 'bottom',
+  controlsContainer: '.compare-slider_2-controls',
+})
