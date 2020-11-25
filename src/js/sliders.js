1eead89459a8c74dddc0c77952a0616ceca752bc
Eeveeee <fx8300gfe@gmail.com>
2020-11-26 01:58:00 +0300

    lazyload

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -4,16 +4,19 @@ tns({
   items: 1,
   navPosition: 'bottom',
   controlsContainer: '.compare-slider_1-controls',
+  lazyload: true,
 })
 tns({
   container: '.compare-slider_2',
   items: 1,
   navPosition: 'bottom',
   controlsContainer: '.compare-slider_2-controls',
+  lazyload: true,
 })
 tns({
   container: '.directions-slider',
   items: 1,
   navPosition: 'bottom',
   controlsContainer: '.directions-slider-controls',
+  lazyload: true,
 })
