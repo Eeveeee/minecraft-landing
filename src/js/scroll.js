741cd6afe1fc20943bd7ff0229cb67df1d6ae609
Eeveeee <fx8300gfe@gmail.com>
2020-11-26 01:42:08 +0300

    little fixes for Apple devices

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -4,13 +4,16 @@ scrollAnchors = [
   document.querySelector('.directions'),
   document.querySelector('.about'),
 ]
-scrollSelectors = document.querySelectorAll('.header-nav__item')
+scrollSelectors = [
+  ...document.querySelectorAll('.header-nav__item'),
+  document.querySelector('.main-roller'),
+]
 scrollSelectors.forEach((selector) => {
   selector.addEventListener('click', (e) => {
     e.preventDefault()
     scrollAnchors.forEach((anchor) => {
       if (selector.getAttribute('href') === anchor.dataset.scroll) {
-        anchor.scrollIntoView({ block: 'center', behavior: 'smooth' })
+        anchor.scrollIntoView({ block: 'nearest', behavior: 'smooth' })
       }
     })
   })
